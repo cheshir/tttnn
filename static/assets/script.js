a4b1e8f05e7ca0bc711259867d5c81def469588a
cheshir <cheshir@programist.ru>
2018-10-09 01:40:20 +0300

    Added result and finishing.

--- static/assets/script.js
+++ static/assets/script.js
@@ -4,16 +4,48 @@ let game = new Vue({
     el: '#game',
     data: {
         cells: [],
+        finished: false,
+        resultMessage: "",
     },
     methods: {
+        init: function() {
+            for (let i = 0; i < 100; i++) {
+                this.cells.push({value: "", active: true})
+            }
+
+        },
+        refresh: function() {
+            // TODO Refresh with O(1)
+            for (let i in this.cells) {
+                this.cells[i].value = "";
+                this.cells[i].active = true;
+            }
+
+            this.resultMessage = "";
+            this.finished = false;
+        },
         makeTurn: function(index) {
+            if (!this.cells[index].active || this.finished) {
+                return
+            }
+
+            // TODO Dummy.
             let nextValue = value == "x" ? "o" : "x";
             this.cells[index].value = nextValue;
             value = nextValue;
+            this.cells[index].active = false;
+
+
+        },
+        win: function() {
+            this.finished = true;
+            this.resultMessage = "WIN!"
+        },
+        lose: function() {
+            this.finished = true;
+            this.resultMessage = "Lose.."
         }
     }
 });
 
-for (let i = 0; i < 100; i++) {
-    game.cells.push({value: ""})
-}
+game.init();
