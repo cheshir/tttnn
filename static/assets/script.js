f6a47d6b5435245e674c82a1a2c3207942dca04b
cheshir <cheshir@programist.ru>
2018-10-09 09:59:24 +0300

    Added win calculation for 3 direction.

--- static/assets/script.js
+++ static/assets/script.js
@@ -1,3 +1,7 @@
+const width = 10;
+const height = 10;
+const neededForWin = 5;
+
 let value = "o";
 
 let game = new Vue({
@@ -9,7 +13,7 @@ let game = new Vue({
     },
     methods: {
         init: function() {
-            for (let i = 0; i < 100; i++) {
+            for (let i = 0; i < width * height; i++) {
                 this.cells.push({value: "", active: true})
             }
 
@@ -35,7 +39,39 @@ let game = new Vue({
             value = nextValue;
             this.cells[index].active = false;
 
+            if (this.isWinning(index)) {
+                this.win()
+            }
+        },
+        isWinning(index) {
+            const expected = this.cells[index].value;
 
+            // Look horizontal.
+            let horizontal = 1 // Center.
+                + this.calculateSiblings(expected, width % index, (i) => i >= 0, (i) => i - 1)  // Look left.
+                + this.calculateSiblings(expected, index + 1, (i) => i % width != 0, (i) => i + 1); // Look right.
+
+            let vertical = 1 // Center.
+                + this.calculateSiblings(expected, index - width, (i) => i >= 0, (i) => i - width) // Look top.
+                + this.calculateSiblings(expected, index + width, (i) => i < this.cells.length, (i) => i + width); // Look bottom.
+
+            let diagonal1 = 1 // Center.
+                + this.calculateSiblings(expected, index - width - 1, (i) => i >= 0 && i % width != width - 1, (i) => i - width - 1) // Look top.
+                + this.calculateSiblings(expected, index + width + 1, (i) => i < this.cells.length && i % width != 0, (i) => i + width + 1); // Look bottom.
+
+            return [horizontal, vertical, diagonal1].some((count) => count >= neededForWin)
+        },
+        calculateSiblings(expected, start, conditionFn, incrementFn) {
+            let count = 0;
+
+            for (let i = start; conditionFn(i); i = incrementFn(i)) {
+                if (this.cells[i].value != expected) {
+                    break;
+                }
+                count++;
+            }
+
+            return count;
         },
         win: function() {
             this.finished = true;
